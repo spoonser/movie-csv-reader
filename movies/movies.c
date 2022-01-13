557c2e3776ff86067b9d8c6e2dcab02b4b6a3a98
spencer <spencerwagner0@gmail.com>
2022-01-13 08:44:05 -0800

    file structure updated

--- movies/movies.c
+++ movies/movies.c
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* Structs */
+struct movie
+{
+	char title[20]; // change for different allocation
+	char languages[2][4]; //change
+	int year;
+	double rating;
+
+	// pointer to next movie goes here
+
+};
+
+/* Function prototypes */
+char readCSV(char);
+
+
+/* Function definitions */
+
+
 int main(int argc, char* argv[])
 {
 	printf("Hello world!\n");
 
 	return 0;
-}
+};
 
-int readCSV()
